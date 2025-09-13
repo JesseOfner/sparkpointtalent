e4b221f52512072a31bcca97a9fb16e88f8cb5df
jesse ofner <JesseOfner@gmail.com>
2025-09-13 12:09:34 +0000

    Generated by Spark: Fix all reported errors.

--- src/pages/CapabilitiesPage.tsx
+++ src/pages/CapabilitiesPage.tsx
@@ -931,9 +931,8 @@ export function CapabilitiesPage() {
           </div>
         </div>
       </section>
-    </div>
-  )
-}      {/* Call to Action */}
+
+      {/* Call to Action */}
       <section className="py-24 bg-gradient-to-br from-primary via-primary to-secondary relative overflow-hidden">
         <div className="absolute inset-0">
           <div className="absolute top-1/3 left-1/3 w-64 h-64 bg-white/5 rounded-full blur-3xl animate-pulse"></div>
